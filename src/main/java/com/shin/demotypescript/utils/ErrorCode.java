806c53920647fde516c6d25efa727cb6723c6231
Trang Nguyen <82458342+trangnguyen0911@users.noreply.github.com>
2021-06-11 16:46:37 +0700

    Update ErrorCode.java

--- src/main/java/com/shin/demotypescript/utils/ErrorCode.java
+++ src/main/java/com/shin/demotypescript/utils/ErrorCode.java
@@ -15,34 +15,33 @@ package com.shin.demotypescript.utils;
  * 01-6-2021  TrangNTT46    Create
  */
 public class ErrorCode {
-	// same name error
-	public static final String SAME_NAME = "Err11";
+    // same name error
+    public static final String SAME_NAME = "Err11";
 	
-	// same email error
-	public static final String SAME_EMAIL = "Err12";
+    // same email error
+    public static final String SAME_EMAIL = "Err12";
 	
-	// same user name error
-	public static final String SAME_USER_NAME = "Err13";
+    // same user name error
+    public static final String SAME_USER_NAME = "Err13";
 	
-	// not exist error
-	public static final String IN_ACTIVE = "Err01";	
+    // not exist error
+    public static final String IN_ACTIVE = "Err01";	
 	
-	// exist error
-	public static final String EXIST_ACTIVE = "Err10";
+    // exist error
+    public static final String EXIST_ACTIVE = "Err10";
 	
-	// no error
-	public static final String SUCCESS = "NoErr";
+    // no error
+    public static final String SUCCESS = "NoErr";
 	
-	// token unable to get user name
-	public static final String ILLEGAL_ARGUMENT_EXCEPTION = "JWT_TOKEN_UNABLE_TO_GET_USERNAME";
+    // token unable to get user name
+    public static final String ILLEGAL_ARGUMENT_EXCEPTION = "JWT_TOKEN_UNABLE_TO_GET_USERNAME";
 	
-	//token expired error
-	public static final String EXPIRED_JWT_EXCEPTION = "JWT_TOKEN_EXPIRED";
+    //token expired error
+    public static final String EXPIRED_JWT_EXCEPTION = "JWT_TOKEN_EXPIRED";
 	
-	// token not start with bearer string error
-	public static final String WARNNING_JWT = "JWT_TOKEN_DOES_NOT_START_WITH_BEARER_STRING";
+    // token not start with bearer string error
+    public static final String WARNNING_JWT = "JWT_TOKEN_DOES_NOT_START_WITH_BEARER_STRING";
 	
-	// unauthoried error
-	public static final String UNAUTHORIZED = "You would need to provide the Jwt Token to Access This resource";
-	
+    // unauthoried error
+    public static final String UNAUTHORIZED = "You would need to provide the Jwt Token to Access This resource";
 }
