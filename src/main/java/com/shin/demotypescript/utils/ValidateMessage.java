1d2154eee71fc605538f9b785a7f93ebfd412eff
Trang Nguyen <82458342+trangnguyen0911@users.noreply.github.com>
2021-06-11 16:47:12 +0700

    Update ValidateMessage.java

--- src/main/java/com/shin/demotypescript/utils/ValidateMessage.java
+++ src/main/java/com/shin/demotypescript/utils/ValidateMessage.java
@@ -15,11 +15,11 @@ package com.shin.demotypescript.utils;
  * 01-6-2021  TrangNTT46    Create
  */
 public class ValidateMessage {
-	public static final String NOT_BLANK = "Please input ";
-	public static final String LENGTH_COURSE_NAME = "Course Name must not exceed 50 characters.";
-	public static final String LENGTH_DESCRIPTION = "Description must not exceed 200 characters.";
-	public static final String LENGTH_USER_NAME = "Username must not exceed 20 characters.";
-	public static final String LENGTH_FULL_NAME = "Full name must not exceed 50 characters.";
-	public static final String EMAIL = "Email is not in correct format";
-	public static final String POSITIVE_NUMBER = " must be a positive integer.";
+    public static final String NOT_BLANK = "Please input ";
+    public static final String LENGTH_COURSE_NAME = "Course Name must not exceed 50 characters.";
+    public static final String LENGTH_DESCRIPTION = "Description must not exceed 200 characters.";
+    public static final String LENGTH_USER_NAME = "Username must not exceed 20 characters.";
+    public static final String LENGTH_FULL_NAME = "Full name must not exceed 50 characters.";
+    public static final String EMAIL = "Email is not in correct format";
+    public static final String POSITIVE_NUMBER = " must be a positive integer.";
 }
