0d96508a82be0539415e63f1fd476b713b3787d9
Trang Nguyen <82458342+trangnguyen0911@users.noreply.github.com>
2021-06-11 15:32:29 +0700

    Update JwtTokenResponse.java

--- src/main/java/com/shin/demotypescript/sercurity/resource/JwtTokenResponse.java
+++ src/main/java/com/shin/demotypescript/sercurity/resource/JwtTokenResponse.java
@@ -17,42 +17,41 @@ import java.io.Serializable;
  * 01-6-2021 TrangNTT46 Create
  */
 public class JwtTokenResponse implements Serializable {
+    private static final long serialVersionUID = 8317676219297719109L;
 
-	private static final long serialVersionUID = 8317676219297719109L;
-
-	/**
-	 * token send to client
-	 */
-	private final String token;
+    /**
+     * token send to client
+     */
+    private final String token;
 	
-	/**
-	 * role send to client
-	 */
-	private final String role;
+    /**
+     * role send to client
+     */
+    private final String role;
 
-	/**
-	 * initial JwtTokenResponse
-	 * @param token
-	 * @param role
-	 */
-	public JwtTokenResponse(String token, String role) {
-		this.token = token;
-		this.role = role;
-	}
+    /**
+     * initial JwtTokenResponse
+     * @param token
+     * @param role
+     */
+    public JwtTokenResponse(String token, String role) {
+        this.token = token;
+        this.role = role;
+    }
 
-	/**
-	 * get token
-	 * @return token
-	 */
-	public String getToken() {
-		return this.token;
-	}
+    /**
+     * get token
+     * @return token
+     */
+    public String getToken() {
+        return this.token;
+    }
 	
-	/**
-	 * get role
-	 * @return role
-	 */
-	public String getRole() {
-		return this.role;
-	}
+    /**
+     * get role
+     * @return role
+     */
+    public String getRole() {
+        return this.role;
+    }
 }
