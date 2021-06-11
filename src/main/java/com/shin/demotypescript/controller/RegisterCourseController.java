da675838ec947f21ade3aec39af507db6b3f7072
Trang Nguyen <82458342+trangnguyen0911@users.noreply.github.com>
2021-06-11 14:27:38 +0700

    Update RegisterCourseController.java

--- src/main/java/com/shin/demotypescript/controller/RegisterCourseController.java
+++ src/main/java/com/shin/demotypescript/controller/RegisterCourseController.java
@@ -34,108 +34,107 @@ import com.shin.demotypescript.service.impl.RegisterCourseServiceImpl;
 @RestController
 @RequestMapping("/registercourse")
 public class RegisterCourseController {
-	@Autowired
-	private RegisterCourseServiceImpl service;
+    @Autowired
+    private RegisterCourseServiceImpl service;
 
-	/**
-	 * controller to get list of registered courses
-	 * 
-	 * @return List<RegisterCourseDTO>
-	 */
-	@GetMapping("/all")
-	public List<RegisterCourseDTO> getAllCourses() {
-	    return service.getAllRegisterCourse();
-	}
+    /**
+    * controller to get list of registered courses
+    * 
+    * @return List<RegisterCourseDTO>
+    */
+    @GetMapping("/all")
+    public List<RegisterCourseDTO> getAllCourses() {
+        return service.getAllRegisterCourse();
+    }
 
-	/**
-	 * controller to sort list of registered courses
-	 * 
-	 * @param field
-	 * @param type
-	 * @return List<RegisterCourseDTO>
-	 */
-	@GetMapping("/sort/{field}/{type}")
-	public List<RegisterCourseDTO> getAllSortedCourses(@PathVariable String field, @PathVariable boolean type) {
-	    return service.getAllSortedRegisterCourse(field, type);
-	}
+    /**
+    * controller to sort list of registered courses
+    * 
+    * @param field
+    * @param type
+    * @return List<RegisterCourseDTO>
+    */
+    @GetMapping("/sort/{field}/{type}")
+    public List<RegisterCourseDTO> getAllSortedCourses(@PathVariable String field, @PathVariable boolean type) {
+        return service.getAllSortedRegisterCourse(field, type);
+    }
 
-	/**
-	 * controller to search registered courses
-	 * 
-	 * @param contentSearch
-	 * @return List<RegisterCourseDTO>
-	 */
-	@GetMapping("/search/{contentSearch}")
-	public List<RegisterCourseDTO> getAllSearchedCourses(@PathVariable String contentSearch) {
-	    return service.getAllFilterRegisterCourse(contentSearch);
-	}
+    /**
+    * controller to search registered courses
+    * 
+    * @param contentSearch
+    * @return List<RegisterCourseDTO>
+    */
+    @GetMapping("/search/{contentSearch}")
+    public List<RegisterCourseDTO> getAllSearchedCourses(@PathVariable String contentSearch) {
+        return service.getAllFilterRegisterCourse(contentSearch);
+    }
 
-	/**
-	 * controller to get list of registered courses by user name
-	 * 
-	 * @param username
-	 * @return List<RegisterCourseDTO>
-	 */
-	@GetMapping("/{username}/courses")
-	public List<RegisterCourseDTO> getAllCoursesByUsername(@PathVariable String username) {
-	    return service.getRegisterCourseByUsername(username);
-	}
+    /**
+    * controller to get list of registered courses by user name
+    * 
+    * @param username
+    * @return List<RegisterCourseDTO>
+    */
+    @GetMapping("/{username}/courses")
+    public List<RegisterCourseDTO> getAllCoursesByUsername(@PathVariable String username) {
+        return service.getRegisterCourseByUsername(username);
+    }
 
-	/**
-	 * controller to search list of registered courses by user name and content
-	 * search
-	 * 
-	 * @param contentSearch
-	 * @param username
-	 * @return List<RegisterCourseDTO>
-	 */
-	@GetMapping("/{username}/search/{contentSearch}")
-	public List<RegisterCourseDTO> getSearchedCoursesByUsername(@PathVariable String contentSearch,
-			@PathVariable String username) {
-	    return service.getFilterRegisterCourseByUsername(username, contentSearch);
-	}
+    /**
+    * controller to search list of registered courses by user name and content
+    * search
+    * 
+    * @param contentSearch
+    * @param username
+    * @return List<RegisterCourseDTO>
+    */
+    @GetMapping("/{username}/search/{contentSearch}")
+    public List<RegisterCourseDTO> getSearchedCoursesByUsername(@PathVariable String contentSearch,
+            @PathVariable String username) {
+        return service.getFilterRegisterCourseByUsername(username, contentSearch);
+    }
 
-	/**
-	 * controller to sort registered course by user name, field, type
-	 * 
-	 * @param username
-	 * @param field
-	 * @param type
-	 * @return List<RegisterCourseDTO>
-	 */
-	@GetMapping("/{username}/sort/{field}/{type}")
-	public List<RegisterCourseDTO> getSortedCoursesByUsername(@PathVariable String username, @PathVariable String field,
-			@PathVariable boolean type) {
-	    return service.getSortedRegisterCourseByUsername(username, field, type);
-	}
+    /**
+    * controller to sort registered course by user name, field, type
+    * 
+    * @param username
+    * @param field
+    * @param type
+    * @return List<RegisterCourseDTO>
+    */
+    @GetMapping("/{username}/sort/{field}/{type}")
+    public List<RegisterCourseDTO> getSortedCoursesByUsername(@PathVariable String username, @PathVariable String field,
+            @PathVariable boolean type) {
+        return service.getSortedRegisterCourseByUsername(username, field, type);
+    }
 
-	/**
-	 * controller to save a new register course
-	 * 
-	 * @param username
-	 * @param courseID
-	 * @return ResponseEntity<String>
-	 */
-	@PostMapping("/{username}/{courseID}")
-	public ResponseEntity<String> saveCourse(@PathVariable String username, @PathVariable long courseID) {
-	    String status = service.saveRegisterCourse(username, courseID);
+    /**
+    * controller to save a new register course
+    * 
+    * @param username
+    * @param courseID
+    * @return ResponseEntity<String>
+    */
+    @PostMapping("/{username}/{courseID}")
+    public ResponseEntity<String> saveCourse(@PathVariable String username, @PathVariable long courseID) {
+        String status = service.saveRegisterCourse(username, courseID);
 
-	    return new ResponseEntity<>(status, HttpStatus.OK);
+        return new ResponseEntity<>(status, HttpStatus.OK);
+    }
 
-	}
+    /**
+    * controller to delete a registered course
+    * 
+    * @param username
+    * @param dto
+    * @return ResponseEntity<String>
+    */
+    @PostMapping("/{username}/cancel")
+    public ResponseEntity<String> deleteRegisteredCourse(@PathVariable String username,
+            @RequestBody RegisterCourseDTO dto) {
+        String status = service.deleteRegisterCourse(dto);
 
-	/**
-	 * controller to delete a registered course
-	 * 
-	 * @param username
-	 * @param dto
-	 * @return ResponseEntity<String>
-	 */
-	@PostMapping("/{username}/cancel")
-	public ResponseEntity<String> deleteRegisteredCourse(@PathVariable String username,
-			@RequestBody RegisterCourseDTO dto) {
-	    String status = service.deleteRegisterCourse(dto);
-
-	    return new ResponseEntity<>(status, HttpStatus.OK);
-	}
+        return new ResponseEntity<>(status, HttpStatus.OK);
+    }
 }
