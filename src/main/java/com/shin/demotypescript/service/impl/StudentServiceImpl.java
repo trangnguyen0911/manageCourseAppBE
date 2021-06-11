acdb95813e930d71ed9f75d2f825dd2145efbd22
Trang Nguyen <82458342+trangnguyen0911@users.noreply.github.com>
2021-06-11 16:31:36 +0700

    Update StudentServiceImpl.java

--- src/main/java/com/shin/demotypescript/service/impl/StudentServiceImpl.java
+++ src/main/java/com/shin/demotypescript/service/impl/StudentServiceImpl.java
@@ -29,107 +29,109 @@ import com.shin.demotypescript.utils.ErrorCode;
  */
 @Service
 public class StudentServiceImpl implements StudentService{
-	@Autowired
-	private StudentRepository repo;
+    @Autowired
+    private StudentRepository repo;
 	
-	@Autowired
-	private RoleRepository roleRepo;
+    @Autowired
+    private RoleRepository roleRepo;
 
-	@Autowired
-	private PasswordEncoder bCryptPassword;
+    @Autowired
+    private PasswordEncoder bCryptPassword;
 
-	/**
-	 * get all active students
-	 * @return List<Student> 
-	 */
-	public List<Student> getAllStudents() {
-		List<Student> students = repo.findActiveStudents();
-		return students;
-	}
+    /**
+     * get all active students
+     * @return List<Student> 
+     */
+    public List<Student> getAllStudents() {
+        List<Student> students = repo.findActiveStudents();
+	
+        return students;
+    }
 
-	/**
-	 * search list of students
-	 * @param contentSearch
-	 * @return List<Student> 
-	 */
-	public List<Student> getFilterStudent(String contentSearch) {
-		List<Student> students = repo.findSearchedStudents(contentSearch);
-		return students;
-	}
+    /**
+     * search list of students
+     * @param contentSearch
+     * @return List<Student> 
+     */
+    public List<Student> getFilterStudent(String contentSearch) {
+        List<Student> students = repo.findSearchedStudents(contentSearch);
+	    
+        return students;
+    }
 
-	/**
-	 * add new student
-	 * @param student
-	 * @return status of action
-	 */
-	public String saveStudent(Student student) {
-		Student existEmailStudent = repo.findByEmailIgnoreCaseAndStatus(student.getEmail(), 1);
-		Student existUserNameStudent = repo.findByUsernameIgnoreCaseAndStatus(student.getUsername(), 1);
+    /**
+     * add new student
+     * @param student
+     * @return status of action
+     */
+    public String saveStudent(Student student) {
+        Student existEmailStudent = repo.findByEmailIgnoreCaseAndStatus(student.getEmail(), 1);
+        Student existUserNameStudent = repo.findByUsernameIgnoreCaseAndStatus(student.getUsername(), 1);
 
-		if (existEmailStudent != null && existUserNameStudent != null) {
-			// exist active student with same email and user name => not save
-			return ErrorCode.SAME_EMAIL + ErrorCode.SAME_USER_NAME;
-		} else if (existEmailStudent != null) {
-			// exist active student with same email => not save
-			return ErrorCode.SAME_EMAIL;
-		} else if (existUserNameStudent != null) {
-			return ErrorCode.SAME_USER_NAME;
-		} else {
-			// save success
-			student.setPassword(bCryptPassword.encode(student.getPassword()));
-			Role userRole = roleRepo.findByName("USER");
-			student.setRole(userRole);
-			repo.save(student);
-			return ErrorCode.SUCCESS;
-		}
-	}
+        if (existEmailStudent != null && existUserNameStudent != null) {
+            // exist active student with same email and user name => not save
+            return ErrorCode.SAME_EMAIL + ErrorCode.SAME_USER_NAME;
+        } else if (existEmailStudent != null) {
+            // exist active student with same email => not save
+            return ErrorCode.SAME_EMAIL;
+        } else if (existUserNameStudent != null) {
+            return ErrorCode.SAME_USER_NAME;
+        } else {
+            // save success
+            student.setPassword(bCryptPassword.encode(student.getPassword()));
+            Role userRole = roleRepo.findByName("USER");
+            student.setRole(userRole);
+            repo.save(student);
+            return ErrorCode.SUCCESS;
+        }
+    }
 
-	/**
-	 * get student by id
-	 * @param id
-	 * @return Student
-	 */
-	public Student getStudentById(long id) {
-		return repo.findById(id).get();
-	}
+    /**
+     * get student by id
+     * @param id
+     * @return Student
+     */
+    public Student getStudentById(long id) {
+        return repo.findById(id).get();
+    }
 
-	/**
-	 * get student by user name
-	 * @param username
-	 * @return Student
-	 */
-	public Student getStudentByUsername(String username) {
-		return repo.findByUsernameIgnoreCaseAndStatus(username, 1);
-	}
+    /**
+     * get student by user name
+     * @param username
+     * @return Student
+     */
+    public Student getStudentByUsername(String username) {
+        return repo.findByUsernameIgnoreCaseAndStatus(username, 1);
+    }
 
-	/**
-	 * update student
-	 * @param username
-	 * @param student
-	 * @return status of action
-	 */
-	public String updateStudent(String username, Student student) {
-		// check active student
-		Student existStudent = this.getStudentByUsername(username);
+    /**
+     * update student
+     * @param username
+     * @param student
+     * @return status of action
+     */
+    public String updateStudent(String username, Student student) {
+        // check active student
+        Student existStudent = this.getStudentByUsername(username);
 
-		if (existStudent == null || existStudent.getStatus() == 0) {
-			// inactive student
-			return ErrorCode.IN_ACTIVE;
-		}
+        if (existStudent == null || existStudent.getStatus() == 0) {
+            // inactive student
+            return ErrorCode.IN_ACTIVE;
+        }
 
-		Student existEmailStudent = repo.findByEmailIgnoreCaseAndStatus(student.getEmail(), 1);
+        Student existEmailStudent = repo.findByEmailIgnoreCaseAndStatus(student.getEmail(), 1);
 
-		if (existEmailStudent != null && !existEmailStudent.getUsername().equalsIgnoreCase(username)) {
-			// exist active student with same email => not save
-			return ErrorCode.SAME_EMAIL;
-		} else {
-			existStudent.setFullname(student.getFullname());
-			existStudent.setEmail(student.getEmail());
-			existStudent.setBirthdate(student.getBirthdate());
-			existStudent.setGender(student.getGender());
-			repo.save(existStudent);
+        if (existEmailStudent != null && !existEmailStudent.getUsername().equalsIgnoreCase(username)) {
+            // exist active student with same email => not save
+            return ErrorCode.SAME_EMAIL;
+        } else {
+            existStudent.setFullname(student.getFullname());
+            existStudent.setEmail(student.getEmail());
+            existStudent.setBirthdate(student.getBirthdate());
+            existStudent.setGender(student.getGender());
+            repo.save(existStudent);
 
-			return ErrorCode.SUCCESS;
-		}
-	}
+            return ErrorCode.SUCCESS;
+        }
+    }
 }
