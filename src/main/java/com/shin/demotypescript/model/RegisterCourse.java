719d6661c0f0b0eb3af41bd50cce3b96781c7147
Trang Nguyen <82458342+trangnguyen0911@users.noreply.github.com>
2021-06-11 14:38:25 +0700

    Update RegisterCourse.java

--- src/main/java/com/shin/demotypescript/model/RegisterCourse.java
+++ src/main/java/com/shin/demotypescript/model/RegisterCourse.java
@@ -43,9 +43,9 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @NoArgsConstructor
 @IdClass(RegisterCourseId.class)
 public class RegisterCourse {	
-	@Id
+    @Id
     @Column(name = "student_id")
-	private Long studentID;
+    private Long studentID;
 
     @Id
     @Column(name = "course_id")
@@ -61,67 +61,65 @@ public class RegisterCourse {
     @JsonIgnore
     private Course course;
 	
-	@Id
-	@Column(name = "register_date", nullable = false)
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	@NotNull
-	private LocalDate registerDate;
+    @Id
+    @Column(name = "register_date", nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotNull
+    private LocalDate registerDate;
 	
-	@Id
-	@Column(name = "register_time", nullable = false)
-	@DateTimeFormat(pattern = "hh:mm:ss")
-	@NotNull
-	private LocalTime registerTime;
+    @Id
+    @Column(name = "register_time", nullable = false)
+    @DateTimeFormat(pattern = "hh:mm:ss")
+    @NotNull
+    private LocalTime registerTime;
 	
-	@Column(name = "status", nullable = false)
-	@Digits(integer = 10, fraction = 0)
-	private int status;
+    @Column(name = "status", nullable = false)
+    @Digits(integer = 10, fraction = 0)
+    private int status;
 
-	/**
-	 * student register a course
-	 * @param student
-	 * @param course
-	 */
-	public RegisterCourse(Student student, Course course) {
-		super();
-		this.student = student;
-		this.course = course;
-		this.registerDate = LocalDate.now();
-		this.registerTime = LocalTime.now();
-		this.status = 1;
-	}
+    /**
+     * student register a course
+     * @param student
+     * @param course
+     */
+    public RegisterCourse(Student student, Course course) {
+        super();
+        this.student = student;
+        this.course = course;
+        this.registerDate = LocalDate.now();
+        this.registerTime = LocalTime.now();
+        this.status = 1;
+    }
 	
-	/**
-	 * student register a course
-	 * @param student
-	 * @param course
-	 * @param studentID
-	 * @param courseID
-	 */
-	public RegisterCourse(Student student, Course course, long studentID, long courseID) {
-		super();
-		this.student = student;
-		this.course = course;
-		this.studentID = studentID;
-		this.courseID = courseID;
-		this.registerDate = LocalDate.now();
-		this.registerTime = LocalTime.now();
-		this.status = 1;
-	}
+    /**
+     * student register a course
+     * @param student
+     * @param course
+     * @param studentID
+     * @param courseID
+     */
+    public RegisterCourse(Student student, Course course, long studentID, long courseID) {
+        super();
+        this.student = student;
+        this.course = course;
+        this.studentID = studentID;
+        this.courseID = courseID;
+        this.registerDate = LocalDate.now();
+        this.registerTime = LocalTime.now();
+        this.status = 1;
+    }
 	
-	/**
-	 * student register a course
-	 * @param studentID
-	 * @param courseID
-	 */
-	public RegisterCourse(long studentID, long courseID) {
-		super();
-		this.studentID = studentID;
-		this.courseID = courseID;
-		this.registerDate = LocalDate.now();
-		this.registerTime = LocalTime.now();
-		this.status = 1;
-	}
-	
+    /**
+     * student register a course
+     * @param studentID
+     * @param courseID
+     */
+    public RegisterCourse(long studentID, long courseID) {
+        super();
+        this.studentID = studentID;
+        this.courseID = courseID;
+        this.registerDate = LocalDate.now();
+        this.registerTime = LocalTime.now();
+        this.status = 1;
+    }	
 }
-
