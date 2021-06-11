23b46357940ececf1b3e7460a415a3085873e0fe
Trang Nguyen <82458342+trangnguyen0911@users.noreply.github.com>
2021-06-11 15:30:46 +0700

    Update JwtTokenRequest.java

--- src/main/java/com/shin/demotypescript/sercurity/resource/JwtTokenRequest.java
+++ src/main/java/com/shin/demotypescript/sercurity/resource/JwtTokenRequest.java
@@ -17,65 +17,64 @@ import java.io.Serializable;
  * 01-6-2021  TrangNTT46    Create
  */
 public class JwtTokenRequest implements Serializable {
+    private static final long serialVersionUID = -5616176897013108345L;
 
-	private static final long serialVersionUID = -5616176897013108345L;
-
-	/**
-	 * user name get from request
-	 */
-	private String username;
+    /**
+     * user name get from request
+     */
+    private String username;
 	
-	/**
-	 * password get from request
-	 */
-	private String password;
+    /**
+     * password get from request
+     */
+    private String password;
 
-	/**
-	 * initial JwtTokenRequest
-	 */
-	public JwtTokenRequest() {
-		super();
-	}
+    /**
+     * initial JwtTokenRequest
+     */
+    public JwtTokenRequest() {
+        super();
+    }
 
-	/**
-	 * initial JwtTokenRequest
-	 * @param username
-	 * @param password
-	 */
-	public JwtTokenRequest(String username, String password) {
-		this.setUsername(username);
-		this.setPassword(password);
-	}
+    /**
+     * initial JwtTokenRequest
+     * @param username
+     * @param password
+     */
+    public JwtTokenRequest(String username, String password) {
+        this.setUsername(username);
+        this.setPassword(password);
+    }
 
-	/**
-	 * get user name
-	 * @return user name
-	 */
-	public String getUsername() {
-		return this.username;
-	}
+    /**
+     * get user name
+     * @return user name
+     */
+    public String getUsername() {
+        return this.username;
+    }
 
-	/**
-	 * set user name
-	 * @param username
-	 */
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    /**
+     * set user name
+     * @param username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	/**
-	 * get password
-	 * @return password
-	 */
-	public String getPassword() {
-		return this.password;
-	}
+    /**
+     * get password
+     * @return password
+     */
+    public String getPassword() {
+        return this.password;
+    }
 
-	/**
-	 * set password
-	 * @param password
-	 */
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    /**
+     * set password
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
